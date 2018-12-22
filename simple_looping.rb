b81986055352c384529eafdb5b4c459c02725bf0
Maximo Bautista <max.bautista97@gmail.com>
2018-12-22 04:57:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,7 +33,7 @@ def while_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  while counter <= number_of_times 
+  while counter < number_of_times 
     counter += 1;
     puts phrase
   end
