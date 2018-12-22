7eb9ffd3ff1c16a7c5c4704c17e4afa972f5044c
Maximo Bautista <max.bautista97@gmail.com>
2018-12-22 04:51:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -58,7 +58,7 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
     range = 1..number_of_times
     for prahse in range 
-      puts prahsew
+      puts prahse
     end
   for 
   
